ac2982f4f66605e6db2f09044a515183421e2194
Alexey Pushkin <alexey.pushkin@gmail.com>
2012-05-26 11:43:10 +0400

    com.amazonaws/aws-java-sdk "1.3.10"

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.bapehbe/sdb "1.0.3-SNAPSHOT"
+(defproject org.clojars.bapehbe/sdb "1.0.4-SNAPSHOT"
   :description "A Clojure library for working with Amazon SimpleDB"
   :dependencies [[log4j/log4j "1.2.15" :exclusions [javax.mail/mail
                                                    javax.jms/jms
                                                    com.sun.jdmk/jmxtools
                                                    com.sun.jmx/jmxri]]
-                 [com.amazonaws/aws-java-sdk "1.1.0"]
+                 [com.amazonaws/aws-java-sdk "1.3.10"]
                  [org.clojure/clojure "1.2.0"]
                  [com.google.code.typica/typica "1.5.2a"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
